2a2f536da4c6570ef67dd59d039b50898fd19bf6
KangJi <kjhmd9726@gmail.com>
2021-05-01 17:06:48 +0900

    [Feature] Phaser 예제를 포함하는 render 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import PhaserRender from './components/PhaserRender';
+
+const App = () => {
   return (
     <div className="App">
       <div>Phaser test</div>
+      <PhaserRender />
     </div>
   );
-}
+};
 
 export default App;
