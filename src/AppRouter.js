2a3d09926e56128685f29d5e117d0844bfd0b3d7
KangJi <kjhmd9726@gmail.com>
2021-05-26 01:58:59 +0900

    [Feat] Page 구현 후 적용

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,27 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Main from './pages/Main';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Game from './pages/Game';
+
 const AppRouter = () => {
   return (
     <Router>
-      <>
-        <Switch>
-          <Route path="/" exact>
-            <div>main</div>
-          </Route>
-          <Route path="/signin">
-            <div>signin</div>
-          </Route>
-          <Route path="/signup">
-            <div>signup</div>
-          </Route>
-          <Route path="/game">
-            <div>game</div>
-          </Route>
-          <Route path="*">
-            <div>404</div>
-          </Route>
-        </Switch>
-      </>
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/game">
+          <Game />
+        </Route>
+        <Route path="*">
+          <div>404</div>
+        </Route>
+      </Switch>
     </Router>
   );
 };
