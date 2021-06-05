a8de5d358c0f4673ed513a397d7b7023d0508eab
KangJi <kjhmd9726@gmail.com>
2021-06-05 23:20:13 +0900

    [feat] 로그인 유지 상태 추가

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -7,6 +8,11 @@ import antiqueUniverseLogo from './../../assets/antique-universe-logo.png';
 import './signin.css';
 
 const SignIn = () => {
+  const [staySigninState, setStaySigninState] = useState(false);
+
+  const handleStaySigninCheckboxChange = () =>
+    setStaySigninState(!staySigninState);
+
   return (
     <div className="signin">
       <div className="signin__main-component">
@@ -25,7 +31,14 @@ const SignIn = () => {
         </div>
         <div className="signin__check-box">
           <FormControlLabel
-            control={<Checkbox name="staySignedIn" color="primary" />}
+            control={
+              <Checkbox
+                name="staySignedIn"
+                color="primary"
+                checked={staySigninState}
+                onChange={handleStaySigninCheckboxChange}
+              />
+            }
             label="로그인 유지"
           />
         </div>
