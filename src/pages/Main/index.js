e0da89fe709abe003103a38b522a40579a1f7daf
KangJi <kjhmd9726@gmail.com>
2021-06-05 22:51:26 +0900

    [feat] Start btn routing 기능 구현

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,14 @@
 import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom';
 import antiqueUniverseLogo from './../../assets/antique-universe-logo.png';
 
 import './main.css';
 
 const Main = () => {
+  const history = useHistory();
+
+  const handleStartBtnClick = () => history.push('/signin');
+
   return (
     <div className="main">
       <div className="main__main-component">
@@ -11,7 +16,11 @@ const Main = () => {
           <img src={antiqueUniverseLogo} alt="antique-universe-logo" />
         </div>
         <div className="main__start-btn">
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleStartBtnClick}
+          >
             시작하기
           </Button>
         </div>
