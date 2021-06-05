7201e5329d0d7126de2080718a67bf5c29e1b4d7
KangJi <kjhmd9726@gmail.com>
2021-06-05 22:20:25 +0900

    [feat] main page jsx 구현

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,21 @@
+import Button from '@material-ui/core/Button';
+import antiqueUniverseLogo from './../../assets/antique-universe-logo.png';
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="main">
+      <div className="main__main-component">
+        <div className="main__logo">
+          <img src={antiqueUniverseLogo} alt="antique-universe-logo" />
+        </div>
+        <div className="main__start-btn">
+          <Button variant="contained" color="primary">
+            시작하기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
