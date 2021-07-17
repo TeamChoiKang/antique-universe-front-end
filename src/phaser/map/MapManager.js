2f67908ba5214633479cfce911e6a7a9aff68108
KangJi <kjhmd9726@gmail.com>
2021-07-17 19:16:20 +0900

    [refactor] map manager 사용하지 않는 메서드 제거

--- src/phaser/map/MapManager.js
+++ src/phaser/map/MapManager.js
@@ -1,14 +1,4 @@
 class MapManager {
-  static createVillageMap(mapBuilder) {
-    return mapBuilder
-      .buildBackground(400, 300)
-      .buildGround(400, 568, 2)
-      .buildGround(600, 400)
-      .buildGround(50, 250)
-      .buildGround(750, 220)
-      .build();
-  }
-
   static createMap(scene, backgroundKey, jsonTileMapKey, tileSetKey) {
     scene.add.image(0, 0, backgroundKey).setOrigin(0, 0);
     const tileMap = scene.make.tilemap({ key: jsonTileMapKey });
