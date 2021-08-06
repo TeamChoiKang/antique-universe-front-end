8331aad3a0470b0e61be2a20ab232afc67d9484f
KangJi <kjhmd9726@gmail.com>
2021-08-07 01:33:19 +0900

    [feature] shop scene socket 이벤트 추가

--- src/phaser/scene/ShopScene/index.js
+++ src/phaser/scene/ShopScene/index.js
@@ -4,6 +4,7 @@ import sky from '@/assets/real-sky.png';
 import TileSet from '@/assets/tile-set.png';
 import Phaser from '@/package/phaser';
 import CharacterFactory from '@/phaser/character/CharacterFactory';
+import CharacterGroup from '@/phaser/character/CharacterGroup';
 import MapManager from '@/phaser/map/MapManager';
 import * as sceneKeys from '@/phaser/scene/sceneKeys';
 import socket from '@/utils/socket';
@@ -29,22 +30,69 @@ class ShopScene extends Phaser.Scene {
   }
 
   create() {
-    const villageMap = MapManager.createMap(this, BACKGROUND_KEY, SHOP_MAP_KEY, TILE_SET_KEY);
+    const shopMap = MapManager.createMap(this, BACKGROUND_KEY, SHOP_MAP_KEY, TILE_SET_KEY);
     const characterFactory = new CharacterFactory(this);
+    const characterGroup = new CharacterGroup(this);
 
-    this.cameras.main.setBounds(0, 0, villageMap.width, villageMap.height);
+    this.cameras.main.setBounds(0, 0, shopMap.width, shopMap.height);
     this.cameras.main.setZoom(1.5);
 
-    const myCharacter = characterFactory.getMyCharacter(
-      450,
-      350,
-      SPRITE_SHEET_KEY,
-      socket.id,
-      () => true,
-    );
+    socket.emit('character:start', 'start');
 
-    this.physics.add.collider(myCharacter, villageMap);
-    this.cameras.main.startFollow(myCharacter, true, 0.5, 0.5);
+    socket.once('character:myCharacter', myCharacterInfo => {
+      const myCharacter = characterFactory.getMyCharacter(
+        myCharacterInfo.x,
+        myCharacterInfo.y,
+        SPRITE_SHEET_KEY,
+        socket.id,
+        character => {
+          socket.emit('character:move', {
+            x: character.x,
+            y: character.y,
+            animation: character.anims.getName(),
+          });
+        },
+      );
+
+      this.physics.add.collider(myCharacter, shopMap);
+      this.cameras.main.startFollow(myCharacter, true, 0.5, 0.5);
+    });
+
+    socket.once('character:currentCharacter', characters => {
+      Object.keys(characters).forEach(index => {
+        characterGroup.add(
+          characterFactory.getAnotherCharacter(
+            characters[index].x,
+            characters[index].y,
+            SPRITE_SHEET_KEY,
+            characters[index].socketId,
+            characters[index].animation,
+          ),
+        );
+      });
+    });
+
+    socket.on('character:newCharacter', characterInfo => {
+      characterGroup.add(
+        characterFactory.getAnotherCharacter(
+          characterInfo.x,
+          characterInfo.y,
+          SPRITE_SHEET_KEY,
+          characterInfo.socketId,
+          characterInfo.animation,
+        ),
+      );
+    });
+
+    socket.on('character:moved', characterInfo => {
+      const movedCharacter = characterGroup.find(characterInfo.socketId);
+      movedCharacter.setPosition(characterInfo.x, characterInfo.y);
+      movedCharacter.anims.play(characterInfo.animation, true);
+    });
+
+    socket.on('character:disconnection', socketId => {
+      characterGroup.remove(socketId);
+    });
   }
 }
 
