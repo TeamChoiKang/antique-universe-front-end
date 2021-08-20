b45d7e41a8812930126c04c5ea21f4ff36284570
KangJi <kjhmd9726@gmail.com>
2021-08-21 01:35:47 +0900

    [feat] shop manager section 나누기

--- src/phaser/scene/ShopManagerScene/index.js
+++ src/phaser/scene/ShopManagerScene/index.js
@@ -4,6 +4,9 @@ const MANAGER_WIDTH = 1650;
 const MANAGER_HEIGHT = 680;
 const MANAGER_BACKGROUND_COLOR = 0xffffff;
 
+const LEFT_SECTION_WIDTH = 660;
+const RIGHT_SECTION_WIDTH = 990;
+
 class ShopManagerScene extends Phaser.Scene {
   constructor(shopScene) {
     super();
@@ -15,6 +18,8 @@ class ShopManagerScene extends Phaser.Scene {
   create() {
     this._registerShutdownEventHandler();
     const background = this._createBackground();
+    const leftSection = this._createLeftSection(background);
+    const rightSection = this._createRightSection(background);
   }
 
   _createBackground() {
@@ -25,8 +30,27 @@ class ShopManagerScene extends Phaser.Scene {
     const background = this.add
       .rectangle(leftMargin, topMargin, MANAGER_WIDTH, MANAGER_HEIGHT, MANAGER_BACKGROUND_COLOR)
       .setOrigin(0);
-
     return background;
+  }
+
+  _createLeftSection(parents) {
+    const leftSectionObject = this.add
+      .rectangle(0, 0, LEFT_SECTION_WIDTH, MANAGER_HEIGHT, 0xff0000, 0.5)
+      .setInteractive();
+
+    Phaser.Display.Align.In.LeftCenter(leftSectionObject, parents);
+
+    return leftSectionObject;
+  }
+
+  _createRightSection(parents) {
+    const rightSectionObject = this.add
+      .rectangle(0, 0, RIGHT_SECTION_WIDTH, MANAGER_HEIGHT, 0x00ff00, 0.5)
+      .setInteractive();
+
+    Phaser.Display.Align.In.RightCenter(rightSectionObject, parents);
+
+    return rightSectionObject;
   }
 
   _registerShutdownEventHandler() {
