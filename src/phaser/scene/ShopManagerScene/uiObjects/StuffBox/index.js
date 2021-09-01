4f7b513d0518bfa36e10e872a1bbbec272bb1dcf
KangJi <kjhmd9726@gmail.com>
2021-09-01 23:52:47 +0900

    [refactor] stuffBox inner html만 변경하도록 수정

--- src/phaser/scene/ShopManagerScene/uiObjects/StuffBox/index.js
+++ src/phaser/scene/ShopManagerScene/uiObjects/StuffBox/index.js
@@ -5,12 +5,15 @@ import * as htmlCreator from './htmlCreator';
 class StuffBox extends Phaser.GameObjects.Container {
   constructor(scene, width, height, stuffs) {
     super(scene, 0, 0);
+    this._width = width;
+    this.height = height;
     this._stuffs = stuffs;
-    this._stuffListHtml = scene.add
-      .dom()
-      .createFromHTML(htmlCreator.createStuffListHtml(width, height, stuffs));
+    this._stuffListHtml = htmlCreator.createStuffListHtml(width, height, stuffs);
+    this._stuffBoxDom = new Phaser.GameObjects.DOMElement(scene).createFromHTML(`<div></div>`);
 
-    this.add(this._stuffListHtml);
+    this._stuffBoxDom.setHTML(this._stuffListHtml);
+
+    this.add(this._stuffBoxDom);
     this.setSize(width, height);
 
     scene.add.existing(this);
@@ -19,13 +22,20 @@ class StuffBox extends Phaser.GameObjects.Container {
   }
 
   _registerEventHandler() {
-    this._stuffListHtml.addListener('click');
+    this._stuffBoxDom.addListener('click');
 
-    this._stuffListHtml.on('click', event => {
-      console.log(event);
+    this._stuffBoxDom.on('click', event => {
+      if (event.action === 'showInfo') {
+        const targetStuff = this._stuffs.find(v => v.getStuffId() === event.stuffId);
+        const stuffInfoHtml = htmlCreator.createStuffInfoHtml(
+          this._width,
+          this._height,
+          targetStuff,
+        );
+
+        this._stuffBoxDom.setHTML(stuffInfoHtml);
+      }
     });
-
-    this.add(this._stuffListHtml);
   }
 }
 
