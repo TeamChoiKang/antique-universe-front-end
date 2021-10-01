7f438bc05aaaedf13b9d00b7456a16281b0de93e
KangJi <kjhmd9726@gmail.com>
2021-10-01 21:56:52 +0900

    [feature] code share

--- src/phaser/scene/VillageScene/index.js
+++ src/phaser/scene/VillageScene/index.js
@@ -1,3 +1,114 @@
+// import dude from '@/assets/dude.png';
+// import sky from '@/assets/real-sky.png';
+// import TileSet from '@/assets/tile-set.png';
+// import VillageTileMap from '@/assets/tileMap/village/village-tile-map.json';
+// import Phaser from '@/package/phaser';
+// import CharacterFactory from '@/phaser/character/CharacterFactory';
+// import CharacterGroup from '@/phaser/character/CharacterGroup';
+// import * as sceneKeys from '@/phaser/scene/sceneKeys';
+// import SceneManager from '@/phaser/scene/SceneManager';
+// import SocketManager from '@/utils/socket/SocketManager';
+
+// const BACKGROUND_KEY = 'backgroud';
+// const VILLAGE_TILE_MAP_KEY = 'villageTileMap';
+// const TILE_SET_KEY = 'tileSet';
+// const SPRITE_SHEET_KEY = 'dude';
+
+// class VillageScene extends Phaser.Scene {
+//   constructor() {
+//     super(sceneKeys.VILLAGE_SCENE_KEY);
+//   }
+
+//   preload() {
+//     this.load.image(TILE_SET_KEY, TileSet);
+//     this.load.tilemapTiledJSON(VILLAGE_TILE_MAP_KEY, VillageTileMap);
+//     this.load.image(BACKGROUND_KEY, sky);
+//     this.load.spritesheet(SPRITE_SHEET_KEY, dude, {
+//       frameWidth: 32,
+//       frameHeight: 48,
+//     });
+//   }
+
+//   create() {
+//     const socket = new SocketManager();
+//     const sceneWithTileMap = SceneManager.setTileMap(
+//       this,
+//       BACKGROUND_KEY,
+//       VILLAGE_TILE_MAP_KEY,
+//       TILE_SET_KEY,
+//     );
+//     const characterFactory = new CharacterFactory(this);
+//     const characterGroup = new CharacterGroup(this);
+//     const createAnotherCharacterAndAppendToCharacterGroup = characterInfo => {
+//       const anotherCharacter = characterFactory.getAnotherCharacter(
+//         characterInfo.x,
+//         characterInfo.y,
+//         SPRITE_SHEET_KEY,
+//         characterInfo.socketId,
+//         characterInfo.animation,
+//       );
+
+//       characterGroup.add(anotherCharacter);
+//     };
+
+//     this.cameras.main.setBounds(0, 0, sceneWithTileMap.width, sceneWithTileMap.height);
+//     this.cameras.main.setZoom(1.5);
+
+//     const sceneChangeKey = this.input.keyboard.addKey('c');
+//     sceneChangeKey.on('down', () => {
+//       socket.removeAllListeners();
+//       SceneManager.changeScene(this, sceneKeys.SHOP_SCENE_KEY);
+//     });
+
+//     socket.emit('map:join', 'village');
+
+//     socket.emit('character:start', 'start');
+
+//     socket.once('character:myCharacter', myCharacterInfo => {
+//       const myCharacter = characterFactory.getGmCharacter(
+//         myCharacterInfo.x,
+//         myCharacterInfo.y,
+//         SPRITE_SHEET_KEY,
+//         socket.id,
+//         character => {
+//           socket.emit('character:move', {
+//             x: character.x,
+//             y: character.y,
+//             animation: character.anims.getName(),
+//           });
+//         },
+//       );
+
+//       this.physics.add.collider(myCharacter, sceneWithTileMap);
+//       this.cameras.main.startFollow(myCharacter, true, 0.5, 0.5);
+//     });
+
+//     socket.once('character:currentCharacter', characters => {
+//       Object.keys(characters).forEach(index => {
+//         createAnotherCharacterAndAppendToCharacterGroup(characters[index]);
+//       });
+//     });
+
+//     socket.on('character:newCharacter', characterInfo => {
+//       createAnotherCharacterAndAppendToCharacterGroup(characterInfo);
+//     });
+
+//     socket.on('character:moved', characterInfo => {
+//       const movedCharacter = characterGroup.find(characterInfo.socketId);
+
+//       if (!movedCharacter) return;
+//       movedCharacter.setPosition(characterInfo.x, characterInfo.y);
+//       movedCharacter.anims.play(characterInfo.animation, true);
+//     });
+
+//     socket.on('character:disconnection', socketId => {
+//       characterGroup.remove(socketId);
+//     });
+//   }
+// }
+
+// export default VillageScene;
+
 import dude from '@/assets/dude.png';
 import sky from '@/assets/real-sky.png';
 import TileSet from '@/assets/tile-set.png';
@@ -13,6 +124,9 @@ const BACKGROUND_KEY = 'backgroud';
 const VILLAGE_TILE_MAP_KEY = 'villageTileMap';
 const TILE_SET_KEY = 'tileSet';
 const SPRITE_SHEET_KEY = 'dude';
+
+const GOOGLE_STUN_SERVER_URL = 'stun:stun.l.google.com:19302';
+const CONFIGURATION = { iceServers: [{ urls: [GOOGLE_STUN_SERVER_URL] }] };
 
 class VillageScene extends Phaser.Scene {
   constructor() {
@@ -104,6 +218,94 @@ class VillageScene extends Phaser.Scene {
     socket.on('character:disconnection', socketId => {
       characterGroup.remove(socketId);
     });
+
+    // testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+
+    const createSenderPeerConnection = stream => {
+      const senderPeerConnection = new RTCPeerConnection(CONFIGURATION);
+
+      senderPeerConnection.onicecandidate = ({ candidate }) => {
+        if (!candidate) return;
+        socket.emit('webRtcAudio:senderIceCandidate', candidate);
+      };
+
+      stream.getTracks().forEach(track => {
+        senderPeerConnection.addTrack(track, stream);
+      });
+
+      return senderPeerConnection;
+    };
+
+    const createSenderOffer = async senderPeerConnection => {
+      const offer = await senderPeerConnection.createOffer();
+      await senderPeerConnection.setLocalDescription(offer);
+      return offer;
+    };
+
+    const appendAudio = stream => {
+      const audio = document.createElement('audio');
+      audio.srcObject = stream;
+      audio.play();
+      document.body.appendChild(audio);
+    };
+
+    const webRtcStart = async () => {
+      let audioSenderPeerConnection;
+
+      socket.on('webRtcAudio:senderAnswer', async answer => {
+        await audioSenderPeerConnection.setRemoteDescription(answer);
+      });
+
+      socket.on('webRtcAudio:senderIceCandidate', async iceCandidate => {
+        await audioSenderPeerConnection.addIceCandidate(iceCandidate);
+      });
+
+      const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+      audioSenderPeerConnection = createSenderPeerConnection(audioStream);
+      const offer = await createSenderOffer(audioSenderPeerConnection);
+      socket.emit('webRtcAudio:senderOffer', offer);
+    };
+
+    const createReceiverPeerConnection = () => {
+      const receiverPeerConnection = new RTCPeerConnection(CONFIGURATION);
+
+      receiverPeerConnection.onicecandidate = ({ candidate }) => {
+        if (!candidate) return;
+        socket.emit('webRtcAudio:receiverIceCandidate', candidate);
+      };
+
+      receiverPeerConnection.ontrack = ({ streams }) => {
+        console.log('ontrack success');
+        appendAudio(streams[0]);
+      };
+
+      return receiverPeerConnection;
+    };
+
+    const createReceiverOffer = async receiverPeerConnection => {
+      const offer = await receiverPeerConnection.createOffer();
+      await receiverPeerConnection.setLocalDescription(offer);
+      return offer;
+    };
+
+    let audioReceiverPeerConnection;
+
+    socket.on('webRtcAudio:connectionCreated', async () => {
+      audioReceiverPeerConnection = createReceiverPeerConnection();
+      const offer = await createReceiverOffer(audioReceiverPeerConnection);
+      socket.emit('webRtcAudio:receiverOffer', offer);
+    });
+
+    socket.on('webRtcAudio:receiverAnswer', async offer => {
+      await audioReceiverPeerConnection.setRemoteDescription(offer);
+    });
+
+    socket.on('webRtcAudio:receiverIceCandidate', async iceCandidate => {
+      await audioReceiverPeerConnection.addIceCandidate(iceCandidate);
+    });
+
+    webRtcStart();
   }
 }
 
