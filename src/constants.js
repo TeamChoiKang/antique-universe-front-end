dc192422c2802c8893a1e9dc9c2974038ed27bbf
joi0104 <jjjjoi0104@gmail.com>
2021-11-28 20:37:18 +0900

    [chore] api path 삭제

--- src/constants.js
+++ src/constants.js
@@ -7,5 +7,5 @@ export const KAKAO_REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
 
 // server
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-export const API_SERVER_URL = `${SERVER_URL}/api`;
-export const SOCKET_SERVER_URL = `${SERVER_URL}/`; // socket.io path가 자동으로 추가되므로 생략
+export const API_SERVER_URL = `${SERVER_URL}`;
+export const SOCKET_SERVER_URL = `${SERVER_URL}`; // socket.io path가 자동으로 추가되므로 생략
