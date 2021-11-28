e299272db50ba1b7bf56262a5fffcf5559b7daa0
joi0104 <jjjjoi0104@gmail.com>
2021-11-28 17:58:47 +0900

    [chore] stun server 추가

--- src/model/WebRtc/PeerConnection/index.js
+++ src/model/WebRtc/PeerConnection/index.js
@@ -5,9 +5,20 @@ class PeerConnection {
       iceServers: [
         {
           urls: [
-            'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
+            'stun.l.google.com:19302',
+            'stun1.l.google.com:19302',
+            'stun2.l.google.com:19302',
+            'stun3.l.google.com:19302',
+            'stun4.l.google.com:19302',
+            'stun.ekiga.net',
+            'stun.ideasip.com',
+            'stun.rixtelecom.se',
+            'stun.schlund.de',
+            'stun.stunprotocol.org:3478',
+            'stun.voiparound.com',
+            'stun.voipbuster.com',
+            'stun.voipstunt.com',
+            'stun.voxgratia.org',
           ],
         },
       ],
